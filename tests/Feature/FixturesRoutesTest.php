24f1bbdfa1596e6b846eff78a69e95e077064410
Micky <mickycheng16@gmail.com>
2021-10-05 14:50:33 +0200

    Added tests

--- tests/Feature/FixturesRoutesTest.php
+++ tests/Feature/FixturesRoutesTest.php
@@ -2,40 +2,13 @@
 
 namespace Tests\Feature;
 
-use App\Models\Player;
-use App\Repositories\PlayerRepository;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
 
-class PlayersRoutesTest extends TestCasePlayersRoutesTest
+class FixturesRoutesTest extends TestCase
 {
     public function testIndex()
     {
-        $this->get(route('players.index'))->assertSuccessful()->assertViewIs('players.index');
+        $this->get(route('fixtures.index'))->assertViewHas('fixtures')->assertSuccessful()->assertViewIs('fixtures.index');
     }
 
-    public function testCreateView()
-    {
-        $this->get(route('players.create'))->assertSuccessful()->assertViewIs('players.create');
-    }
-
-    public function testCreatePlayer()
-    {
-        $this->post(route('players.store'), ['name' => 'TestPlayer'])->assertSuccessful();
-    }
-
-    public function testDeletePlayer()
-    {
-        $player = new Player(['name' => 'TestDeletePlayer']);
-        $player->save();
-        $this->delete(route('players.destroy', ['player' => $player->id]))->assertSuccessful();
-    }
-
-    public function testUpdatePlayer()
-    {
-        $player = Player::factory()->create();
-        $attrs = collect(Player::factory()->make()->getAttributes())->except('image')->toArray();
-        $attrs['id'] = $player->id;
-        $this->put(route('players.update'), $attrs)->assertSuccessful();
-    }
 }
