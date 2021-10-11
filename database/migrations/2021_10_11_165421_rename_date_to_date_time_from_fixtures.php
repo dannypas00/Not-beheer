28f95bbf38541b17ef7b75284d7db81a0722a1c2
Micky <mickycheng16@gmail.com>
2021-10-11 18:56:48 +0200

    ??2

--- database/migrations/2021_10_11_165421_rename_date_to_date_time_from_fixtures.php
+++ database/migrations/2021_10_11_165421_rename_date_to_date_time_from_fixtures.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class ChangeDateToDateTimeInFixtures extends Migration
+class RenameDateToDateTimeFromFixtures extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,7 @@ class ChangeDateToDateTimeInFixtures extends Migration
     public function up()
     {
         Schema::table('fixtures', function (Blueprint $table) {
-            $table->dateTime('date')->change();
+            $table->renameColumn('date', 'date_time');
         });
     }
 
