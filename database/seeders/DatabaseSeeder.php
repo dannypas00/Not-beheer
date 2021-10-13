6e86f9c7337070c40e2766fed19a21526207733f
dannypas00 <danny.pas@student.nhlstenden.com>
2021-10-13 11:52:01 +0200

    Various fixes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,6 +7,7 @@ use App\Models\Country;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Log;
 use PHPUnit\Framework\Constraint\Count;
 
 class DatabaseSeeder extends Seeder
@@ -23,7 +24,14 @@ class DatabaseSeeder extends Seeder
 //        $this->call(GamesSeeder::class);
 //        $this->call(SetsSeeder::class);
 //        $this->call(LegsSeeder::class);
-        DB::unprepared(File::get('database/seeders/countries-dump.sql'));
-        DB::unprepared(File::get('database/seeders/cities-dump.sql'));
+        if (env('DB_CONNECTION') === 'mysql') {
+            Log::info('MYSQL import');
+            DB::unprepared(File::get('database/seeders/countries-dump.sql'));
+            DB::unprepared(File::get('database/seeders/cities-dump.sql'));
+        } elseif (env('DB_CONNECTION' === 'sqlite')) {
+            Log::info('SQLITE import');
+            DB::unprepared(File::get('database/seeders/countries-dump.sqlite'));
+            DB::unprepared(File::get('database/seeders/cities-dump.sqlite'));
+        }
     }
 }
