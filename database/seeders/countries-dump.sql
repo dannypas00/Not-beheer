47e8dc9c66752d9eaa91aa5be9cd7fa3bc6440ec
dannypas00 <danny.pas@student.nhlstenden.com>
2021-10-13 11:05:22 +0200

    Removed autoincrement from sql

--- database/seeders/countries-dump.sql
+++ database/seeders/countries-dump.sql
@@ -23,7 +23,7 @@ DROP TABLE IF EXISTS `countries`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `countries` (
-  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `id` bigint unsigned NOT NULL,
   `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `iso3` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `iso2` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
