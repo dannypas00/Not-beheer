8b144082839a61b36954cb87fac0607e657be7ae
Micky <mickycheng16@gmail.com>
2021-10-13 16:22:13 +0200

    delete white line

--- app/Http/Controllers/FixtureController.php
+++ app/Http/Controllers/FixtureController.php
@@ -69,6 +69,5 @@ class FixtureController extends AbstractController
     public function destroy(Fixture $fixture): RedirectResponse
     {
         return app(FixtureHandler::class)->destroy($fixture);
-
     }
 }
