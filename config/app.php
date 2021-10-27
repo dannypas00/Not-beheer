1f0b94c28aaaaa692d05c20f924c076ba297fe19
dannypas00 <dannypas00@outlook.com>
2021-10-27 14:37:29 +0200

    Removed old shit

--- config/app.php
+++ config/app.php
@@ -175,10 +175,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
-        //Custom providers
-        App\Providers\EasyGraphServiceProvider::class,
-
     ],
 
     /*
